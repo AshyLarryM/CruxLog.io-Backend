e01f705859d7586c3b34373b61d900c0a3f6bd9c
AshyLarryM <LarryM093@gmail.com>
2024-11-11 20:54:33 -0800

    api/user/[userId]/session/getAll.  Return grades based on gradingPrerefence boolean.

--- src/app/api/user/[userId]/session/route.ts
+++ src/app/api/user/[userId]/session/route.ts
@@ -3,6 +3,7 @@ import { db } from '@/drizzle/db';
 import { climb, session, users } from "@/drizzle/schema";
 import { eq, and, desc } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
+import { boulderGradeMapping, routeGradeMapping } from "@/lib/types";
 
 export async function POST(req: NextRequest, { params }: { params: { userId: string } }) {
     revalidatePath(req.url);
@@ -49,6 +50,7 @@ export async function POST(req: NextRequest, { params }: { params: { userId: str
 
 
 export async function GET(req: NextRequest, { params }: { params: { userId: string } }) {
+    console.time("Route Execution Time");
     revalidatePath(req.url);
 
     try {
@@ -59,17 +61,33 @@ export async function GET(req: NextRequest, { params }: { params: { userId: stri
             return NextResponse.json({ message: "User does not exist" }, { status: 400 });
         }
 
-        const [userSession] = await db.select().from(session).where(and(eq(session.userId, userId), eq(session.completed, false))).orderBy(desc(session.createdAt)).limit(1);
+        const { gradingPreference } = user;
+
+        const [userSession] = await db.select()
+            .from(session)
+            .where(and(eq(session.userId, userId), eq(session.completed, false)))
+            .orderBy(desc(session.createdAt))
+            .limit(1);
 
         if (!userSession) {
             return NextResponse.json({ message: "No Active Session Found" });
         }
-
         const climbs = await db.select().from(climb).where(eq(climb.sessionId, userSession.id));
-        return NextResponse.json({ message: "Active Session Found", session: userSession, climbs: climbs }, { status: 200 })
+
+        const transformedClimbs = climbs.map(climb => {
+            const gradeMapping = climb.type === "Boulder" ? boulderGradeMapping : routeGradeMapping;
+            const grade = gradingPreference ? gradeMapping[climb.grade] : climb.grade;
+
+            return {
+                ...climb,
+                grade,
+            };
+        });
+
+        return NextResponse.json({ message: "Active Session Found", session: userSession, climbs: transformedClimbs }, { status: 200 });
     } catch (error) {
-        console.error("Error fetching session and Climbs: ", error);
-        return NextResponse.json({ message: "failed to fetch session and climbs: ", error }, { status: 500 });
+        console.error("Error fetching session and climbs: ", error);
+        return NextResponse.json({ message: "Failed to fetch session and climbs", error }, { status: 500 });
     }
 }
 
