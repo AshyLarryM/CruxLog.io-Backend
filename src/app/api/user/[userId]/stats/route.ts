b52a3d343a82c47150451f3b2c74c43bcbd7ca39
AshyLarryM <LarryM093@gmail.com>
2024-11-15 17:50:56 -0800

    get user grade stats.

--- src/app/api/user/[userId]/stats/route.ts
+++ src/app/api/user/[userId]/stats/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from '@/drizzle/db';
 import { users, session, climb } from "@/drizzle/schema";
-import { eq, count } from "drizzle-orm";
+import { eq, or, desc, and } from "drizzle-orm";
+import { boulderGradeMapping, routeGradeMapping } from "@/lib/types";
 import { revalidatePath } from "next/cache";
 
 export const dynamic = 'force-dynamic';
@@ -15,29 +16,64 @@ export async function GET(req: NextRequest, { params }: { params: { userId: stri
             return NextResponse.json({ message: "userId is required" }, { status: 400 });
         }
 
-        const userExists = await db
-            .select()
-            .from(users)
-            .where(eq(users.userId, userId));
-
-        if (!userExists.length) {
+        const [user] = await db.select().from(users).where(eq(users.userId, userId));
+        if (!user) {
             return NextResponse.json({ message: "User not found" }, { status: 404 });
         }
 
-        const climbsByTypeAndStyle = await db
+        const { gradingPreference } = user;
+
+
+        const highestBoulderGrade = await db
             .select({
+                grade: climb.grade,
                 type: climb.type,
-                style: climb.style,
-                count: count(climb.id)
             })
             .from(climb)
             .innerJoin(session, eq(climb.sessionId, session.id))
             .innerJoin(users, eq(session.userId, users.userId))
-            .where(eq(users.userId, userId))
-            .groupBy(climb.type, climb.style);
+            .where(and(eq(users.userId, userId), eq(climb.type, "Boulder")))
+            .orderBy(desc(climb.grade))
+            .limit(1);
 
-        return NextResponse.json({ message: `User: ${userId} stats fetched`, userStats: climbsByTypeAndStyle }, { status: 200 });
+        const highestRouteGrade = await db
+            .select({
+                grade: climb.grade,
+                type: climb.type,
+            })
+            .from(climb)
+            .innerJoin(session, eq(climb.sessionId, session.id))
+            .innerJoin(users, eq(session.userId, users.userId))
+            .where(and(
+                eq(users.userId, userId),
+                or(eq(climb.type, "Lead"), eq(climb.type, "Top Rope"))
+            ))
+            .orderBy(desc(climb.grade))
+            .limit(1);
+
+        const transformedGrades = {
+            Boulder: highestBoulderGrade[0]
+                ? {
+                    ...highestBoulderGrade[0],
+                    grade: gradingPreference
+                        ? boulderGradeMapping[highestBoulderGrade[0].grade] || highestBoulderGrade[0].grade
+                        : highestBoulderGrade[0].grade,
+                }
+                : null,
+            Route: highestRouteGrade[0]
+                ? {
+                    ...highestRouteGrade[0],
+                    grade: gradingPreference
+                        ? routeGradeMapping[highestRouteGrade[0].grade] || highestRouteGrade[0].grade
+                        : highestRouteGrade[0].grade,
+                    type: "Route",
+                }
+                : null,
+        };
+
+        return NextResponse.json({ message: `User: ${userId} grades fetched`, userGrades: transformedGrades }, { status: 200 });
     } catch (error) {
+        console.error("Error fetching user grades: ", error);
         return NextResponse.json({ message: "An error occurred", error: error }, { status: 500 });
     }
 }
