1a1501e20e20767efb1f77d7c10cbccff93c679b
AshyLarryM <LarryM093@gmail.com>
2024-11-18 14:25:54 -0800

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/api/user/:slug*']);
+const isProtectedRoute = createRouteMatcher(['/api/user/:slug*', 'api/createUser']);
 
 export default clerkMiddleware(async (auth, req) => {
     // console.log("Middlware Accessed For: ", req.url);
